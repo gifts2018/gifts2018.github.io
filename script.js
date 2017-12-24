98fee906ef3975bf8bd99a4d694320b9428f0524
Irina <irbris@mail.ru>
2017-12-24 08:07:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 var card = document.getElementById("card");
-var cardBack = document.querySelector(".back")
+var cardFront = document.querySelector(".front");
+var cardBack = document.querySelector(".back");
 var textCard = "";
 var gifts = document.querySelectorAll(".gifts li");
 var audio = document.querySelector("audio");
 
 card.addEventListener("click", function() {
   card.classList.toggle("flipped");
-  audio.pause();
+  if(card.classList.contains("flipped")) {
+    audio.currentTime = 0;
+    audio.play();
+  } else {
+    audio.pause();
+  }  
 });
 
 for(var i = 0; i < gifts.length; i++) {
   gifts[i].addEventListener("click", function(){
-    card.classList.toggle("flipped");
-    textCard = this.dataset.text;
-    cardBack.innerHTML = textCard + "";
-    audio.currentTime = 0;
-    audio.play();
-  });
-}
-
-for(var i = 0; i < gifts.length; i++) {
-  gifts[i].addEventListener("click", function(){
-    this.classList.add("sway");
+    if(!card.classList.contains("flipped")) {
+      card.classList.add("flipped");
+      this.classList.add("sway");
+      textCard = this.dataset.text;
+      cardBack.innerHTML = textCard + "";
+      audio.currentTime = 0;
+      audio.play();
+    }    
   });
 }
