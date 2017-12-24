728b65e2b1f8a4f80577456ad69c83dd14e0a406
Irina <irbris@mail.ru>
2017-12-24 17:36:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,63 @@
-var card = document.getElementById("card");
-var cardFront = document.querySelector(".front");
-var cardBack = document.querySelector(".back");
-var textCard = "";
+var card = document.querySelector(".card");
+var p = document.querySelector(".container button"); 
 var gifts = document.querySelectorAll(".gifts li");
 var audio = document.querySelector("audio");
+var textCard = "";
 
 card.addEventListener("click", function() {
-  card.classList.toggle("flipped");
-  if(card.classList.contains("flipped")) {
-    audio.currentTime = 0;
-    audio.play();
-  } else {
-    audio.pause();
+  if(this.classList.contains("initial")) {
+    this.classList.remove("initial")
+    gifts.forEach(function(gift) {
+      gift.style.display = "flex";
+      gift.classList.add("scale");
+      return;
+    })
   }  
+  
+  card.classList.remove("flipOut");
+  card.classList.add("flipped");
+  p.classList.remove("scaleOut");
+  setTimeout(() => {
+    this.style.background = "url('img/bg2.png')";
+    p.innerHTML = "С Новым годом";
+    p.style.display = "block";
+    p.classList.add("scale");
+    audio.currentTime = 0;
+    audio.play(); 
+  }, 3000);
 });
+
+p.addEventListener("click", function() {
+  card.classList.remove("fliped");
+  card.classList.add("flipOut");
+  p.classList.remove("scale");
+  p.classList.add("scaleOut");
+  setTimeout(() => {
+    card.style.background = "url('img/bg1.png')";
+    p.innerHTML = "";
+    p.style.display = "none";    
+    audio.pause();
+  }, 1000);
+})
+
 
 for(var i = 0; i < gifts.length; i++) {
   gifts[i].addEventListener("click", function(){
-    if(!card.classList.contains("flipped")) {
+    this.classList.remove("scale");
+    if(card.classList.contains("flipOut") && !this.classList.contains("sway")) {
+      card.classList.remove("flipOut");
       card.classList.add("flipped");
       this.classList.add("sway");
-      textCard = this.dataset.text;
-      cardBack.innerHTML = textCard + "";
-      audio.currentTime = 0;
-      audio.play();
-    }    
+      p.classList.remove("scaleOut");
+      setTimeout(() => {
+        card.style.background = "url('img/bg2.png')";
+        textCard = this.dataset.text;
+        p.innerHTML = textCard + "";
+        p.style.display = "block";
+        p.classList.add("scale");
+        audio.currentTime = 0;
+        audio.play();
+      }, 3000);      
+    } 
   });
 }
